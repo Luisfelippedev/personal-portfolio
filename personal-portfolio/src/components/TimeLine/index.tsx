df83aa199dd1d02b8ce9f53611ac6446c2acb833
Luisffelipe <luis2015.sjp@gmail.com>
2024-02-23 17:16:00 -0300

    feat: Criando componente TimeLine

---
+++ personal-portfolio/src/components/TimeLine/index.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import {
+  Button,
+  Timeline,
+  TimelineBody,
+  TimelineContent,
+  TimelineItem,
+  TimelinePoint,
+  TimelineTime,
+  TimelineTitle,
+} from "flowbite-react";
+import { useEffect, useState } from "react";
+import { HiArrowNarrowRight, HiCalendar } from "react-icons/hi";
+
+export const TimeLine = () => {
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth <= 1100);
+    };
+
+    handleResize(); // Definir inicialmente com base na largura da tela
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <Timeline horizontal={isSmallScreen ? false : true}>
+      <TimelineItem>
+        <TimelinePoint icon={HiCalendar}></TimelinePoint>
+        <TimelineContent style={{ marginTop: "1rem" }}>
+          <TimelineTime>2021</TimelineTime>
+          <TimelineTitle>Faculdade</TimelineTitle>
+          <TimelineBody>
+            Iniciando minha graduação em análise e desenvolvimento de sistemas
+            através do Instituto Federal da Paraíba.
+          </TimelineBody>
+        </TimelineContent>
+      </TimelineItem>
+      <TimelineItem >
+        <TimelinePoint icon={HiCalendar}></TimelinePoint>
+        <TimelineContent style={{ marginTop: "1rem" }}>
+          <TimelineTime>2022</TimelineTime>
+          <TimelineTitle>JavaScript</TimelineTitle>
+          <TimelineBody>
+            Explorando de forma mais detalhada a linguagem JavaScript e
+            aprofundando-me em seus aspectos tanto no desenvolvimento front-end
+            quanto no back-end.
+          </TimelineBody>
+        </TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelinePoint icon={HiCalendar}></TimelinePoint>
+        <TimelineContent style={{ marginTop: "1rem" }}>
+          <TimelineTime>2023</TimelineTime>
+          <TimelineTitle>Bibliotecas e Projetos</TimelineTitle>
+          <TimelineBody>
+            Conheçendo o grande poder das bibliotecas em JavaScript, e
+            desenvolvendo projetos pessoais de forma concisa.
+          </TimelineBody>
+        </TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelinePoint icon={HiCalendar}></TimelinePoint>
+        <TimelineContent style={{ marginTop: "1rem" }}>
+          <TimelineTime>2024</TimelineTime>
+          <TimelineTitle>Até o momento</TimelineTitle>
+          <TimelineBody>
+            Em busca da minha primeira oportunidade como desenvolvedor.
+          </TimelineBody>
+        </TimelineContent>
+      </TimelineItem>
+      
+    </Timeline>
+  );
+};
