a8697b5b5461a8f4992816d847784e6fa68d0964
Luisffelipe <luis2015.sjp@gmail.com>
2024-02-23 17:17:13 -0300

    fix: alterando componente Button

--- personal-portfolio/src/components/Button/index.tsx
+++ personal-portfolio/src/components/Button/index.tsx
@@ -5,18 +5,17 @@ import SouthIcon from "@mui/icons-material/South";
 type Props = {
   children: ReactNode;
   color: "primary" | "secondary";
-  isIcon?: boolean;
   iconSize?: "small" | "large"
 };
 
-export const Button = ({ children, color, isIcon, iconSize}: Props) => {
+export const Button = ({ children, color, iconSize}: Props) => {
   return (
     <button
       style={{
         width: "100%",
         height: "100%",
         justifyContent: "center",
-        padding: isIcon ? "3% 10%" : "3% 9%",
+        padding: "3% 9%",
       }}
       className={
         color == "primary" ? styles.ButtonPrimary : styles.ButtonSecondary
@@ -24,7 +23,6 @@ export const Button = ({ children, color, isIcon, iconSize}: Props) => {
     >
       <div className={styles.contentButton}>
         {children}
-        {isIcon ? <SouthIcon fontSize='medium' /> : null}
       </div>
     </button>
   );
